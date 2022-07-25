72d2acd0bbacc0aec84ec56370942729ee13d08e
Shubham Kumar <sktovisit@gmail.com>
2022-07-25 12:25:25 +0530

    NFT_contract_&_tailwind_configed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "92b2b29ce72a4a0b9db2d09032b7ccaa";
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
+
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.9",
 };
