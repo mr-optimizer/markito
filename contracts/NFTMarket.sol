3bf20c460aee9c4527fc4129f3b14909a0e297de
Shubham Kumar <sktovisit@gmail.com>
2022-07-28 18:31:33 +0530

    state_variable_added_and_createMarketItem_fun_created

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -1,3 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//prevents re-entrancy attacks
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _itemIds; //total number of items ever created
+    Counters.Counter private _itemsSold; //total number of items sold
+
+    address payable owner; //owner of the smart contract
+    //people have to pay to puy their NFT on this marketplace
+    uint256 listingPrice = 0.025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller; //person selling the nft
+        address payable owner; //owner of the nft
+        uint256 price;
+        bool sold;
+    }
+
+    //a way to access values of the MarketItem struct above by passing an integer ID
+    mapping(uint256 => MarketItem) private idMarketItem;
+
+    //log message (when Item is sold)
+    event MarketItemCreated(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    /// @notice function to get listingprice
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    /// @notice function to create market item
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be above zero");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        _itemIds.increment(); //add 1 to the total number of items ever created
+        uint256 itemId = _itemIds.current();
+
+        idMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender), //address of the seller putting the nft up for sale
+            payable(address(0)), //no owner yet (set owner to empty address)
+            price,
+            false
+        );
+
+        //transfer ownership of the nft to the contract itself
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        //log this transaction
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+    }
+}
