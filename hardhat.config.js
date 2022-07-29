949fd3587e386887383884924d68e4fa45513ffa
Shubham Kumar <sktovisit@gmail.com>
2022-07-29 23:08:35 +0530

    contracts_testing_done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
 const privateKey = fs.readFileSync(".secret").toString();
 const projectId = "92b2b29ce72a4a0b9db2d09032b7ccaa";
 /** @type import('hardhat/config').HardhatUserConfig */
 
-
-
 module.exports = {
   networks: {
-    hardhat: {
+    hardhat:{
       chainId: 1337
     },
     mumbai: {
@@ -18,7 +16,7 @@ module.exports = {
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
       accounts: [privateKey]
-    }
+    },
   },
-  solidity: "0.8.9",
+  solidity: "0.8.10",
 };
